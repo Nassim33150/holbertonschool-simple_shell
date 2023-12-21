9c18d95bf9a47d32ea8843b2e513e1a3e1a9cd86
Mamuche <marionlaroche17@gmail.com>
2023-12-21 19:57:37 +0100

    main.c betty

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - fonction main.
+ * @ac: number of arguments.
+ * @argv: array of arguments.
+ * Return: Always 0.
+*/
 int main(int ac, char **argv)
 {
 
